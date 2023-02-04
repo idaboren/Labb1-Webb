6dafa7977da249d814a807d02cf6ad45d7057bbb
idaboren <idaboren@gmail.com>
2023-02-04 14:04:43 +0100

    added stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+// const animals = [`cat`, `dog`, `fish`, `bear`];
+// // console.log(animals);
+// // animals.splice(0, 0, `fox`);
+// // console.log(animals);
+// // animals.push(`bird`);
+// // console.log(animals);
+// // let dog = animals.find((a) => a === `dog`);
+// // console.log(dog);
+// const moreAnimals = [`snail`, `snake`];
+
+// for (let i = 0; i < moreAnimals.length; i++) {
+//   animals.splice(2, 0, moreAnimals[i]);
+// }
+
+// // moreAnimals.forEach((a) => animals.splice(2, 2, a));
+
+// console.log(animals);
