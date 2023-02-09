32720433899c9705e029757d1e76f7818cfcda97
idaboren <idaboren@gmail.com>
2023-02-09 18:03:22 +0100

    ändrade beställning

--- site.js
+++ site.js
@@ -1,7 +1,8 @@
 class Product {
-  constructor(name, description, price) {
+  constructor(name, image, altText, description, price) {
     this.name = name;
-    // this.image = image;
+    this.image = image;
+    this.altText = altText;
     this.description = description;
     this.price = price;
   }
@@ -10,18 +11,24 @@ class Product {
 let product1 = new Product("Bakelse", "Innehåller mjöl", 20000);
 product1 = {
   name: "Bakelse",
+  image: "400.jpg",
+  altText: "Bakelse",
   description: "Innehåller mjöl.",
   price: 20000,
 };
 
 const product2 = {
   name: "Bröd",
+  image: "400.jpg",
+  altText: "Bröd",
   description: "Innehåller också mjöl.",
   price: 40000,
 };
 
 const product3 = {
   name: "Tårta",
+  image: "400.jpg",
+  altText: "Tårta",
   description: "Innehåller grädde.",
   price: 60000,
 };
@@ -35,20 +42,29 @@ showCart();
 function showProducts() {
   for (const product of products) {
     const card = document.createElement("div");
-    // const cardImage = document.createElement("img");
+    const cardImage = document.createElement("img");
     const cardBody = document.createElement("div");
     const cardTitle = document.createElement("h2");
     const cardFooter = document.createElement("div");
     const readMoreButton = document.createElement("button");
     const addToCartButton = document.createElement("button");
 
+    cardImage.src = product.image;
+    cardImage.alt = product.altText;
     card.classList.add("card", "p-0");
-    // cardImage.classList.add("card-img-top");
+    cardImage.classList.add("card-img-top");
     cardBody.classList.add("card-body");
     cardTitle.classList.add("card-title");
-    cardFooter.classList.add("card-footer", "bg-white", "border-0", "p-1");
-    readMoreButton.classList.add("btn", "btn-light");
-    addToCartButton.classList.add("btn", "btn-light");
+    cardFooter.classList.add(
+      "card-footer",
+      "bg-white",
+      "border-0",
+      "p-1",
+      "pt-0",
+      "text-center"
+    );
+    readMoreButton.classList.add("btn", "btn-light", "m-1");
+    addToCartButton.classList.add("btn", "btn-light", "m-1");
 
     //cardImage
     cardTitle.innerText = product.name;
@@ -63,7 +79,7 @@ function showProducts() {
     const cardList = document.querySelector("#cardList");
     cardFooter.append(readMoreButton, addToCartButton);
     cardBody.append(cardTitle);
-    card.append(cardBody, cardFooter); //cardImage
+    card.append(cardImage, cardBody, cardFooter); //cardImage
     cardList.append(card);
   }
 }
@@ -77,9 +93,9 @@ function showCart() {
 
     row.classList.add("row");
     productName.classList.add("col");
-    removeFromCartButton.classList.add("btn", "btn-light", "col");
+    removeFromCartButton.classList.add("btn", "btn-close", "col-end");
 
-    removeFromCartButton.innerText = "Ta bort";
+    //removeFromCartButton.innerText = "Ta bort";
     productName.innerText = product.name;
 
     removeFromCartButton.onClick = (product) => {
